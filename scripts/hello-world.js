33b9f2a17717b4caf1f48d0518c478f7794b3a6d
mvizm2015 <mariaviz2015@gmail.com>
2018-02-12 22:08:56 -0500

    Restrictive

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -1,6 +1,8 @@
 angular.module('todoListApp')
 .directive('helloWorld', function() {
   return {
-    template: "This is the hello world directive"
+    template: "This is the hello world directive",
+    restrict: "E"
+
   }
 });
